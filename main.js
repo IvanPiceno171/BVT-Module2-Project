cf350afb26e6fc5a2a72eaedbdaca8e5d80632aa
unknown <you@example.com>
2023-04-05 19:47:06 -0700

    search by name & reset button

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 // function fetchPoke() {
-    
+
 //     // let promise1 =  fetch('https://pokeapi.co/api/v2/pokemon/'+poke)
-    
+
 //     let x = 1
 //     while(x<=151){
-    //         x++;
-    //         let promise1 =  fetch(`https://pokeapi.co/api/v2/pokemon/${x}`)
-    
-    //         promise1.then((response)=>{
-        //             return response.json();
-        //         }).then((data)=>{
-            //             const pokemon = {};
+//         x++;
+//         let promise1 =  fetch(`https://pokeapi.co/api/v2/pokemon/${x}`)
+
+//         promise1.then((response)=>{
+//             return response.json();
+//         }).then((data)=>{
+//             const pokemon = {};
 //         pokemon['name'] = data.name;
 //         pokemon['id'] = data.id;
 //         pokemon['image'] = data.sprites['front_shiny']
 //         console.log(pokemon)
 //     }).catch((error)=>{
-    //         console.log('Error:')
-    
+//         console.log('Error:')
+
 //     })
 // }
 //   }
@@ -25,16 +25,16 @@
 // fetchPoke()
 
 
-const cont = document.getElementById('container1')
-  const fetchPoke = async ()=>{
+const contain = document.getElementById('container1')
+const fetchPoke = async () => {
     let x = 0
     const allPokemon = []
-    while(x<151){
+    while (x < 151) {
         x++;
-        try{
+        try {
             const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${x}`);
             const data = await response.json();
-            
+
             const pokemon = {}; //empty object 
 
             // creates object for every iteration
@@ -45,25 +45,58 @@ const cont = document.getElementById('container1')
 
             allPokemon.push(pokemon) // pushes each obj into array
 
-        }catch(error){
+        } catch (error) {
             console.error(error)
         }
     }
     displayPoke(allPokemon);
-  }
+}
 
-  fetchPoke();
+fetchPoke();
 
 
-  function displayPoke(pokemon){
-    console.log(pokemon)
-    const allPoke = pokemon.map((poke)=>{
-       return ` <li class="card">
+function displayPoke(pokemon) {
+
+    const allPoke = pokemon.map((poke) => {
+        return `
+        <div class = "individual-pokemon"> 
+       <li class="card">
        <img class="card-image" src="${poke.image}"/>
-       <h2 class="card-title">${poke.id}. ${poke.name}</h2>
-    </li>
+       <h2 class="card-title">${poke.id}.${poke.name}</h2>
+        </li>
+        </div>
         `
     }).join('')
-    cont.innerHTML = allPoke;
-  }
+    contain.innerHTML = allPoke;
+
+    const searchBar = document.getElementById('searchbox');
+    searchBar.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let inputbox = document.querySelector('.inputText').value
+    for (x of pokemon) {
+        //   search bar
+            if (inputbox === x.name) {
+                // console.log('match')
+                return contain.innerHTML = `
+                <div class = "individual-pokemon"> 
+               <li class="card">
+               <img class="card-image" src="${x.image}"/>
+               <h2 class="card-title">${x.id}. ${x.name}</h2>
+                </li>
+                </div>`
+            }
+        }
+        if(inputbox !== x.name){
+            alert('no such pokemon found')
+        }
+
+        })
+
+
+        // Reset button to reset all pokemon
+
+
+    }
+
+
 
