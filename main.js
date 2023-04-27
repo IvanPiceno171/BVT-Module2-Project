b42bfafea571214a5ed034360dafc4c74a77f407
unknown <you@example.com>
2023-04-26 19:17:39 -0700

    date added

--- main.js
+++ main.js
@@ -1,30 +1,33 @@
-// function fetchPoke() {
+// timer
 
-//     // let promise1 =  fetch('https://pokeapi.co/api/v2/pokemon/'+poke)
-
-//     let x = 1
-//     while(x<=151){
-//         x++;
-//         let promise1 =  fetch(`https://pokeapi.co/api/v2/pokemon/${x}`)
-
-//         promise1.then((response)=>{
-//             return response.json();
-//         }).then((data)=>{
-//             const pokemon = {};
-//         pokemon['name'] = data.name;
-//         pokemon['id'] = data.id;
-//         pokemon['image'] = data.sprites['front_shiny']
-//         console.log(pokemon)
-//     }).catch((error)=>{
-//         console.log('Error:')
-
-//     })
-// }
-//   }
-
-// fetchPoke()
+const date = new Date()
+let year = date.getFullYear()
+let month = date.getMonth()
+let day = date.getDate()
 
 
+let timerContainer = document.querySelector(".clock-div");
+let yearLi = document.createElement('div')
+let monthdiv = document.createElement('div')
+let dayLi = document.createElement('div')
+yearLi.innerText =year
+monthdiv.innerText = month
+dayLi.innerText = day
+if(month === 3){
+    monthdiv.innerText = "April "
+}
+if(month === 4){
+    monthdiv.innerText = "May"
+}
+timerContainer.append(monthdiv)
+timerContainer.append(dayLi)
+timerContainer.append(yearLi)
+
+
+
+
+
+// Pokemon rendered
 const contain = document.getElementById('container1')
 
 const fetchPoke = async () => {
@@ -52,10 +55,7 @@ const fetchPoke = async () => {
     }
     displayPoke(allPokemon);
 }
-
-setTimeout(()=>{   
-    fetchPoke();
-},10)
+fetchPoke();
 
 
 function displayPoke(pokemon) {
