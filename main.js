69c51f4ad868789e936a43377994539d40323954
unknown <ivan.piceno171@gmail.com>
2023-06-22 22:10:44 -0700

    adds pokemons name to localStorage

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-// timer
 
+// import from localStorage
+// import { setItem } from "./localStorage"
+
+// timer
 const date = new Date()
 let year = date.getFullYear()
 let month = date.getMonth()
@@ -13,6 +16,9 @@ let dayDiv = document.createElement('div')
 yearDiv.innerText = year
 monthDiv.innerText = month
 dayDiv.innerText = day
+//
+//  MAKE THIS INTO
+// A SWITCH STATEMENT
 if (month === 3) {
     monthDiv.innerText = "April "
 }
@@ -52,25 +58,46 @@ const fetchPoke = async () => {
 }
 fetchPoke();
 
+// FUNCTION TO ADD to localStorage
+
 
 function displayPoke(pokemon) {
-    const allPoke = pokemon.map((poke) => {
+    const allPoke = pokemon.map((poke, index) => {
         return `
         <div class = "individual-pokemon"> 
-       <li class="card">
-       <img class="card-image" src="${poke.image}"/>
-       <h2 class="card-title">${poke.id}.${poke.name}</h2>
+        <li class="card">
+        <img class="card-image" src="${poke.image}"/>
+        <h2 class="card-title">${poke.id}.${poke.name}</h2>
+        <button class="addButton" data-index='${index}' name='${poke.name}'>Add to Pokedex</button>
         </li>
         </div>
         `
     }).join('')
     pokeContainer.innerHTML = allPoke;
+    
+
+     
+    const addButton = document.querySelectorAll('.addButton'); //grabs all buttons NodeList
+    addButton.forEach((button)=>{
+        button.addEventListener('click', (e)=>{
+            // const pokeindex = e.target.dataset.index; //dataset is grabs data-index value
+            const toPokedex = e.target.name
+            let caughtArray = localStorage.getItem("caught");
+      caughtArray = caughtArray ? JSON.parse(caughtArray) : []; // Retrieve stored array or initialize as empty if none exists
+      caughtArray.push(toPokedex); // Add the caught Pokemon to the array
+
+      localStorage.setItem("caught", JSON.stringify(caughtArray)); // Store the updated array in localStorage
+
+      console.log(caughtArray);
+        })
+    });
+
 
     const searchBar = document.getElementById('searchbox');
     searchBar.addEventListener('submit', (e) => {
         e.preventDefault();
         let inputbox = document.querySelector('.inputText').value
-
+        
         let pokemonHTML = '';
         for (x of pokemon) {
             //   search bar
@@ -79,16 +106,19 @@ function displayPoke(pokemon) {
                 <li class="card">
                 <img class="card-image" src="${x.image}"/>
                 <h2 class="card-title">${x.id}. ${x.name}</h2>
-                 </li>
-                 </div>`;
+                <button class='addButton' data-index='${x.id}'>Add to Pokedex</button>
+                </li>
+                </div>`;
             }
             pokeContainer.innerHTML = pokemonHTML
         }
-
+        
     })
+    
 
-
+    
 }
+
 
 
 
