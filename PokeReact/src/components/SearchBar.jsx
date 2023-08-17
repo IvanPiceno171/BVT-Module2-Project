233402aa0112414ced65c83baae5a0d54934dc8e
unknown <ivan.piceno171@gmail.com>
2023-08-17 13:58:12 -0700

    basic express added

--- PokeReact/src/components/SearchBar.jsx
+++ PokeReact/src/components/SearchBar.jsx
@@ -1,6 +1,52 @@
 import React from 'react'
 
 export default function SearchBar() {
+
+
+
+  // const searchBar = document.getElementById('searchbox');
+
+  //   searchBar.addEventListener('submit', (e) => {
+  //       e.preventDefault();
+  //       let inputbox = document.querySelector('.inputText').value
+
+  //       let pokemonHTML = '';
+
+  //   for (x of pokemon) {
+  //       //   search bar
+  //           if (inputbox.toLowerCase() === x.name.toLowerCase()) {
+  //               // console.log('match')
+  //               pokemonHTML = `
+  //               <div class = "individual-pokemon"> 
+  //              <li class="card">
+  //              <img class="card-image" src="${x.image}"/>
+  //              <h2 class="card-title">${x.id}. ${x.name}</h2>
+  //               </li>
+  //               </div>`
+  //           }
+  //           else if (x.name.toLowerCase().includes(inputbox.toLowerCase())) {
+  //               pokemonHTML += `<div class = "individual-pokemon"> 
+  //               <li class="card">
+  //               <img class="card-image" src="${x.image}"/>
+  //               <h2 class="card-title">${x.id}. ${x.name}</h2>
+  //                </li>
+  //                </div>`;
+  //             }
+
+  //           contain.innerHTML = pokemonHTML
+  //       }
+
+  //       })
+
+
+
+
+
+
+
+
+
+
   return (
     <div>
         <div className="form-holder">
